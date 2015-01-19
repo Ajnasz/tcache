191d32fb3f7335bbcd3b09a016b789a125237d37
Lajos Koszti <ajnasz@ajnasz.hu>
2015-01-20 00:01:49 +0100

    Add documentation

--- tcache.go
+++ tcache.go
@@ -1,15 +1,23 @@
+// Package tcache implements a simple library, to store []byte in memory for a
+// given amount of time
 package tcache
 
 import(
 	"time"
 )
 
+// TCacheItem represents an item which is stored in the cace
+// Name will be used to acces to a item
+// Values should be the actual value
+// Expire should be a time when the cache expired
 type TCacheItem struct {
 	Name string
 	Value []byte
 	Expire time.Time
 }
 
+// IsExpired returns true, if current Now() is later than the time was defined
+// in Expire
 func (item *TCacheItem) IsExpired() bool {
 	return item.Expire.UnixNano() / int64(time.Millisecond) < time.Now().UnixNano() / int64(time.Millisecond)
 }
@@ -20,20 +28,25 @@ type TCollection interface {
 	GetAll() []TCacheItem
 }
 
+// TCacheCollection is a struct which stores the cache items
 type TCacheCollection struct {
 	HitCount int
 	MissCount int
 	Items map[string]TCacheItem
 }
 
+// Add adds a new item to the cache
 func (c *TCacheCollection) Add(item TCacheItem) {
 	c.Items[item.Name] = item
 }
 
+// Returns all items in the cache
 func (c *TCacheCollection) GetAll() map[string]TCacheItem {
 	return c.Items
 }
 
+// Returns one specific item from the cache
+// Also increments HitCount or MissCount
 func (c *TCacheCollection) Get(name string) (TCacheItem, bool) {
 	var found bool = false
 	var item TCacheItem
@@ -49,10 +62,12 @@ func (c *TCacheCollection) Get(name string) (TCacheItem, bool) {
 	return item, found
 }
 
+// Removes item from cache
 func (c *TCacheCollection) Remove(name string) {
 	delete(c.Items, name)
 }
 
+// Removes expired cache items from TCacheCollection
 func RemoveExpired(collection *TCacheCollection) {
 	for key, item := range collection.Items {
 		if item.IsExpired() {
@@ -69,6 +84,9 @@ func ScheduleRemoveExpired(collection *TCacheCollection, freq time.Duration) {
 	}
 }
 
+// Creates a cache collections
+// Calls ScheduleRemoveExpired to remove expired items in every 100
+// milliseconds
 func CreateCache() *TCacheCollection {
 	items := make(map[string]TCacheItem)
 
